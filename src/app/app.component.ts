2bc2414590c203d33f4b00d99ca8b4df97d551e0
AlahR0077 <ar.raja2020@gmail.com>
2021-06-17 11:31:50 +0500

    uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lms';
+  title = 'portal';
+  name = 'AlahR0077';
+  testVar = 'Testing 2 way data binding';
+  save() {
+    alert('You Clicked')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeleteDataComponent } from './delete-data/delete-data.component';
+import { StudentMarksComponent } from './student-marks/student-marks.component';
+import { SubmitAssignmentComponent } from './submit-assignment/submit-assignment.component';
+import { UploadAssignmentComponent } from './upload-assignment/upload-assignment.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteDataComponent,
+    StudentMarksComponent,
+    SubmitAssignmentComponent,
+    UploadAssignmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
